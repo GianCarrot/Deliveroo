6b63ed819b9072a2ed83eeacfd5163996c2238af
GianRoty <123582204+GianRoty@users.noreply.github.com>
2026-04-06 18:43:39 +0200

    Added Prerequirements

--- BDI_Agent/deliveroo/connection.js
+++ BDI_Agent/deliveroo/connection.js
@@ -2,7 +2,9 @@ import { DjsConnect } from '@unitn-asa/deliveroo-js-sdk'
 
 export function connect() {
     const host = process.env.HOST;
-    const socket = DjsConnect(host);
+    const token = proccess.env.TOKEN;
+
+    const socket = DjsConnect(host, token);
 
     return socket;
 }
